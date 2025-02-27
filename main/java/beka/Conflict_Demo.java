e35e11b1fc9837afb61a3029fdedc05cfc1604f6
Beka <BekaCoder@gmail.com>
2025-02-27 13:08:51 +0600

    regress fixes

--- main/java/beka/Conflict_Demo.java
+++ main/java/beka/Conflict_Demo.java
@@ -2,6 +2,6 @@ package beka;
 
 public class Conflict_Demo {
     public static void main(String[] args) {
-        System.out.println("Fix my test");
+        System.out.println("Regress fixes");
     }
 }
